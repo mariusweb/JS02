db6d9cb63ec2320ba480373d53e087f0139bb5c4
mariusweb <39734577+mariusweb@users.noreply.github.com>
2021-01-07 18:21:58 +0200

    03-6 task

--- 03-5/JS/script.js
+++ 03-5/JS/script.js
@@ -3,7 +3,6 @@ let texts = document.querySelectorAll('.lead');
 
 texts.forEach(text => {
     text.addEventListener('focus', function(e){
-        // e.preventDefault();
         text.classList.add('lead1');
     });
     text.addEventListener('blur', e => {
